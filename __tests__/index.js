3bc2f72e2deb339ef2ebdc27545e17975e66e626
Brennan Fee <github@todaytechsoft.biz>
2018-04-06 23:56:53 -0500

    Fixing test to work.

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,7 +3,8 @@
 const showdown = require('../index.js');
 
 test('showdown-markdown-loader:index', () => {
-    expect(showdown.call({}, "# Heading")).toBe(
-        "<!DOCTYPE HTML>\n<html>\n<head>\n<meta charset=\"utf-8\">\n</head>\n<body>\n<h1 id=\"heading\">Heading</h1>\n</body>\n</html>"
+    expect(showdown.call({ completeHTMLDocument: true }, "# Heading")).toBe(
+      //        "<!DOCTYPE HTML>\n<html>\n<head>\n<meta charset=\"utf-8\">\n</head>\n<body>\n<h1 id=\"heading\">Heading</h1>\n</body>\n</html>"
+      "<h1 id=\"heading\">Heading</h1>"
     );
 });
