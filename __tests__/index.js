1170bc92175c0e85824452489d34d3149da516b1
Brennan Fee <bfee@uship.com>
2018-04-09 11:47:49 -0500

    Fixed the passing of options for tests.

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,10 +1,16 @@
-'use strict';
+"use strict";
+const showdown_loader = require("../index.js");
 
-const showdown = require('../index.js');
+test("showdown-markdown-loader:index", () => {
+  expect(showdown_loader.call({ query: {} }, "# Heading")).toBe(
+    '<h1 id="heading">Heading</h1>'
+  );
+});
 
-test('showdown-markdown-loader:index', () => {
-    expect(showdown.call({ completeHTMLDocument: true }, "# Heading")).toBe(
-      //        "<!DOCTYPE HTML>\n<html>\n<head>\n<meta charset=\"utf-8\">\n</head>\n<body>\n<h1 id=\"heading\">Heading</h1>\n</body>\n</html>"
-      "<h1 id=\"heading\">Heading</h1>"
-    );
+test("showdown-markdown-loader:index", () => {
+  expect(
+    showdown_loader.call({ query: { completeHTMLDocument: true } }, "# Heading")
+  ).toBe(
+    '<!DOCTYPE HTML>\n<html>\n<head>\n<meta charset="utf-8">\n</head>\n<body>\n<h1 id="heading">Heading</h1>\n</body>\n</html>'
+  );
 });
